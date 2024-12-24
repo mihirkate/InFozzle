fe35cf7b041221541b4bcbdadbb2e4e6305e8855
mihirkate <mihirkate13@gmail.com>
2024-12-24 20:27:53 +0530

    Add responsive to navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,16 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import ButtonComponent from "./custom/ButtonComponent";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleLogin = () => {
     console.log("clicked On LOGIN");
   };
 
   const handleSignup = () => {
     console.log("clicked On Signup");
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -25,19 +31,41 @@ const Navbar = () => {
           />
         </a>
       </div>
-      <ul className="flex space-x-8">
+
+      <div className="md:hidden flex items-center">
+        <button onClick={toggleMenu} className="focus:outline-none">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <ul
+        className={`md:flex space-x-8 ${isOpen ? "block" : "hidden"} md:block`}
+      >
         <li>
-          <a href="#" className="text-black hover:">
+          <a href="#" className="text-black hover:underline">
             Investments
           </a>
         </li>
         <li>
-          <a href="#" className="text-black hover:">
+          <a href="#" className="text-black hover:underline">
             Owners/Brokers
           </a>
         </li>
         <li>
-          <a href="#" className="text-black hover:">
+          <a href="#" className="text-black hover:underline">
             Lorem Ipsum
           </a>
         </li>
@@ -50,6 +78,28 @@ const Navbar = () => {
         />
         <ButtonComponent text="Signup" onClick={handleSignup} />
       </div>
+
+      {isOpen && (
+        <div className="absolute top-16 left-0 w-full bg-white shadow-lg md:hidden">
+          <ul className="flex flex-col space-y-4 p-4">
+            <li>
+              <a href="#" className="text-black hover:underline">
+                Investments
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-black hover:underline">
+                Owners/Brokers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-black hover:underline">
+                Lorem Ipsum
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
